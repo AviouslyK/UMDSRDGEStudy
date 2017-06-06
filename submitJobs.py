01a3f28b6fa6e6450a5ed18c1fa7ff6d872695d8
AviouslyK <avi.fs.kahn@gmail.com>
2017-06-06 14:37:23 -0400

    Update submitJobs.py

--- submitJobs.py
+++ submitJobs.py
@@ -2,23 +2,25 @@
 import os, sys
 import shlex, subprocess
 from datetime import datetime, date, time
+from time import sleep
+import shutil
 #sys.path.append(os.path.abspath(os.path.curdir))
 
 JobTime = datetime.now()
 fTag = JobTime.strftime("%Y%m%d_%H%M%S")
-sTag = "AlphaSource_1kevts"
+sTag = "run10"
 dirname = "jobs/%s_%s"%(sTag,fTag)
-InFile = "AlphaSource.in"#"HERadDam.in"
 DetType = "1" #rod
+logFile = "0606.log"
 
 try:
     os.makedirs(dirname)
 except:
     pass
 
-ProdTag = "UMDSRDGEStudy-00-00-02_20170331"
-OutDir  = "/data/users/jengbou/G4Results"
-WorkDir = "/home/jengbou/workspace/UserCode/geant4/UMDSRDGEStudy-build"
+ProdTag = "run10_20170606"
+OutDir  = "/home/kahn/PhysHonr268n/CMSSW_5_3_30/Research/G4/honrgeant/UMDSRDGEStudy-build/Absdata"
+WorkDir = "/home/kahn/PhysHonr268n/CMSSW_5_3_30/Research/G4/honrgeant/UMDSRDGEStudy-build/"
 
 try:
     os.makedirs(OutDir)
@@ -36,43 +38,83 @@ except:
     pass
 
 
+
 #########################################
-# make sure OutDir is the same in main.cc
+#make sure OutDir is the same in main.cc
 #########################################
 condor_script_template = """
 universe = vanilla
-Executable = ./LYSim.sh
+Executable = ./CMS.sh
 +IsLocalJob = true
 Should_transfer_files = NO
 Requirements = TARGET.FileSystemDomain == "privnet"
 Output = %(OUTDIR)s/%(MYPREFIX)s/logs/%(FILENAME)s_sce_$(cluster)_$(process).stdout
 Error  = %(OUTDIR)s/%(MYPREFIX)s/logs/%(FILENAME)s_sce_$(cluster)_$(process).stderr
 Log    = %(OUTDIR)s/%(MYPREFIX)s/logs/%(FILENAME)s_sce_$(cluster)_$(process).condor
-Arguments = %(WORKDIR)s %(INPUT)s %(OUTDIR)s/%(MYPREFIX)s/%(FILENAME)s %(DETTYPE)s
+Arguments = %(WORKDIR)s %(INPUT)s %(FILENAME)s %(DETTYPE)s %(LOGFILE)s
 Queue 1
-
+ 
 """
-#########################################
+######################%(WORKDIR)s %(INPUT)s %(FILENAME)s %(DETTYPE)s###################
+# %(OUTDIR)s/%(MYPREFIX)s/
 
-kw = {}
+Abs = 199
+counter = 1  
+for counter in range (1,10):
+    #Abs = Abs + 1
+    #counter = counter + 1
+    #Copy photontet to a new dummy file
+    shutil.copy2('photontest.mac', 'photest' '%s' '.mac' % counter)
+    
+    # Read in the file
+    with open('photest' '%s' '.mac' % counter, 'r') as file :
+        filedata = file.read()
 
-kw["MYPREFIX"]  = ProdTag
-kw["WORKDIR"]   = WorkDir
-kw["OUTDIR"]    = OutDir
-kw["INPUT"]     = InFile
-kw["FILENAME"]  = sTag
-kw["DETTYPE"]   = DetType
+        # Replace original abslength with abslength + counter 
+    AbsPlusSome = Abs + counter
+    j = str(AbsPlusSome)
+    i = str(Abs)
+    filedata = filedata.replace(i, j)
 
-script_str = condor_script_template % kw
-f = open("%s/condor_jobs_%s_G4Sim.jdl"%(dirname,sTag), 'w')
-f.write(script_str)
-f.close()
+    # Write the file out again
+    with open('photest' '%s' '.mac' % counter, 'w') as file:
+        file.write(filedata)
 
-condorcmd = "condor_submit %s/condor_jobs_%s_G4Sim.jdl"%(dirname,sTag)
-print 'condorcmd: ', condorcmd
-print 'Executing condorcmd'
-p=subprocess.Popen(condorcmd, shell=True)
-p.wait()
+    # Define new infile
+    InFile = 'photest' '%s' '.mac' % counter
+     
+    
+    kw = {}
 
-print "Histos output dir: %s/%s"%(OutDir,ProdTag)
+    kw["MYPREFIX"]  = ProdTag
+    kw["WORKDIR"]   = WorkDir
+    kw["OUTDIR"]    = OutDir
+    kw["INPUT"]     = InFile
+    kw["FILENAME"]  = sTag
+    kw["DETTYPE"]   = DetType
+    kw["LOGFILE"]   = logFile
 
+    script_str = condor_script_template % kw
+    f = open("%s/condor_jobs_%s_G4Sim.jdl"%(dirname,sTag), 'w')
+    f.write(script_str)
+    f.close()
+
+    condorcmd = "condor_submit %s/condor_jobs_%s_G4Sim.jdl"%(dirname,sTag)
+    print 'condorcmd: ', condorcmd
+    print ('Executing condorcmd %s' % str(counter))
+
+    p=subprocess.Popen(condorcmd, shell=True)
+    p.wait()
+   
+        
+    print "\n"
+    print "Histos output dir: %s/%s"%(OutDir,ProdTag)
+
+#########################
+### in separate script###
+#num = counter
+#counter2 = 0
+# Now delete the dummy files
+#while (counter2 < num):
+#    counter2 = counter2 + 1
+#    os.remove('photest' '%s' '.mac' % counter2)
