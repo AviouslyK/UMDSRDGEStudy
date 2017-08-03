b24b67ff042a026005299a651d54a1ec9041d103
AviouslyK <avi.fs.kahn@gmail.com>
2017-08-03 17:39:55 -0400

    Updated submitJobs.py
    Can now submit thousands of jobs only a few at a time

--- submitJobs.py
+++ submitJobs.py
@@ -9,17 +9,17 @@ import numpy as np
 
 JobTime = datetime.now()
 fTag = JobTime.strftime("%Y%m%d_%H%M%S")
-sTag = "10mmDataCollection"
+sTag = "saved4mmDataCollection"
 dirname = "jobs/%s_%s"%(sTag,fTag)
 DetType = "1" #rod
-logFile = "0629.log"
+logFile = "0731.log"
 
 try:
     os.makedirs(dirname)
 except:
     pass
 
-ProdTag = "Run1_20170706"
+ProdTag = "Run3_20170803"
 OutDir  = "/home/kahn/PhysHonr268n/CMSSW_5_3_30/Research/G4/honrgeant/UMDSRDGEStudy-build/Absdata"
 WorkDir = "/home/kahn/PhysHonr268n/CMSSW_5_3_30/Research/G4/honrgeant/UMDSRDGEStudy-build/"
 
@@ -33,8 +33,7 @@ try:
 except:
     pass
 
-try:
-    os.makedirs(OutDir+"/"+ProdTag+"/logs")
+try:    os.makedirs(OutDir+"/"+ProdTag+"/logs")
 except:
     pass
 
@@ -59,43 +58,49 @@ Queue 1
 ######################    %(LOGFILE)s  ###################
 # %(OUTDIR)s/%(MYPREFIX)s/
 
+
+#################################################################
+################## For Changing Wavelength ######################
+#################################################################
+
 #starting and ending wavelenght
-#Initial = 2.1
+#Initial = 3.542
 
-#Final = 3.6
+#Final = 2.254
 
-#waveNUM = 35
+#waveNUM = 100
+
 #array that contains the different wavelengths being tested                          
 #Arr = np.linspace(Initial,Final,num=waveNUM)
 
-#Arr = [2.9590, 2.9520, 2.9450, 2.9380, 2.9311, 2.9242, 2.9173, 2.8833, 2.8502, 2.8178, 2.7862, 
-#       2.7552, 2.7249, 2.6953, 2.6663, 2.6380, 2.6102, 2.5830, 2.5564, 2.5303, 2.5047, 2.4797, 
-#       2.4551, 2.4311, 2.4075, 2.3843, 2.3616, 2.3393, 2.3175, 2.2960, 2.2749, 2.2543] 
-
-
-#3.5424, 3.4925, 3.4440, 3.3968, 3.3509, 3.3062, 3.2627, 3.2204, 3.2037, 3.1955, 3.1791, 
- #      3.1709, 3.1629, 3.1548, 3.1468, 3.1388, 3.1309, 3.1230, 3.1152, 3.1074, 3.0996, 3.0919, 
-  #     3.0842, 3.0765, 3.0689, 3.0613, 3.0538, 3.0463, 3.0388, 3.0314, 3.0240, 3.0166, 3.0093, 
-   #    3.0020, 2.9948, 2.9876, 2.9804, 2.9732, 2.9661
-
-
-
 #for q in range(0,len(Arr)):
 
+
+
+
+################################################################
+################## For Changing AbsLength ######################
+################################################################
+
 #initial absorption length
-AbsIN = 4.0
+AbsIN = 0.5
 
 #final absorption length
-AbsFI = 6.0
+AbsFI = 50.0
 
 #total number of jobs being submitted 
-jobNUM = 175
+jobNUM = 1000
 
 #array that contains the different abs lengths being tested                          
 Arr = np.linspace(AbsIN,AbsFI,num=jobNUM)
-
+# used to allow the submission of thousands of jobs, 20 at a time
+pauser = 1;
 for q in range(len(Arr)):
     
+    if (pauser % 20 == 0):
+        sleep(600);
+
+    pauser = pauser + 1; 
     # Creating new file                                                              
     shutil.copy2('photontest.mac', 'photest' '%s' '.mac' % q)
 
@@ -138,6 +143,8 @@ for q in range(len(Arr)):
     p=subprocess.Popen(condorcmd, shell=True)
     p.wait()
    
-        
+    
     print "\n"
     #print "Histos output dir: %s/%s"%(OutDir,ProdTag)
+
+#print(Arr)
