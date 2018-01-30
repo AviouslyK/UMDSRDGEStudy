5cebdff5b9fca0345d3d70fec7a74b396999ba13
AviouslyK <avi.fs.kahn@gmail.com>
2018-01-29 21:45:53 -0500

    Update submit jobs
    added random seed information

--- submitJobs.py
+++ submitJobs.py
@@ -9,17 +9,17 @@ import numpy as np
 
 JobTime = datetime.now()
 fTag = JobTime.strftime("%Y%m%d_%H%M%S")
-sTag = "saved4mmDataCollection"
+sTag = "FitSimulation150mm"
 dirname = "jobs/%s_%s"%(sTag,fTag)
 DetType = "1" #rod
-logFile = "0731.log"
+logFile = "1128.log"
 
 try:
     os.makedirs(dirname)
 except:
     pass
 
-ProdTag = "Run3_20170803"
+ProdTag = "Run1_20171128"
 OutDir  = "/home/kahn/PhysHonr268n/CMSSW_5_3_30/Research/G4/honrgeant/UMDSRDGEStudy-build/Absdata"
 WorkDir = "/home/kahn/PhysHonr268n/CMSSW_5_3_30/Research/G4/honrgeant/UMDSRDGEStudy-build/"
 
@@ -64,15 +64,38 @@ Queue 1
 #################################################################
 
 #starting and ending wavelenght
-#Initial = 3.542
+#Initial = 3.5424
 
-#Final = 2.254
+#Final = 2.1752
 
-#waveNUM = 100
+#waveNUM = 25
 
 #array that contains the different wavelengths being tested                          
-#Arr = np.linspace(Initial,Final,num=waveNUM)
+#Arr = [3.5424, 3.4925, 3.4440, 3.3968, 3.3509, 3.3062, 3.2627, 3.2204,
+#       3.1791, 3.1388, 3.0996, 3.0613, 3.0240, 2.9876, 2.9520, 2.9173,
+#       2.8834, 2.8502, 2.8178, 2.7862, 2.7552, 2.7249, 2.6953, 2.6663,
+#       2.6380, 2.6102, 2.5830, 2.5564, 2.5303, 2.5047, 2.4797, 2.4551, 
+#       2.4311, 2.4075, 2.3843, 2.3616, 2.3393, 2.3175, 2.2960, 2.2749, 
+#       2.2543, 2.2339, 2.2140, 2.1944, 2.1752, 2.1562, 2.1377, 2.1194, 
+#       2.1014, 2.0838, 2.0664];
 
+#Arr = [3.542, 3.493, 3.444, 3.397, 3.351, 3.306, 3.263, 3.22, 3.179, 3.139, 
+#       3.1, 3.061, 3.024, 2.988, 2.952, 2.917, 2.883, 2.85, 2.818, 2.786, 2.755, 
+#       2.725, 2.695, 2.666, 2.638];
+
+#Arr = [3.542, 3.493, 3.444, 3.397, 3.351, 3.306, 3.263, 3.220, 3.179, 3.163, 3.155, 3.147, 
+#       3.139, 3.131, 3.123, 3.115, 3.107, 3.100, 3.092, 3.084, 3.077, 3.069, 3.061, 3.054, 
+#       3.046, 3.039, 3.031, 3.024, 3.017, 3.009, 3.002, 2.995, 2.988, 2.952, 2.883, 2.818, 
+#       2.755, 2.695, 2.638, 2.583, 2.530, 2.480, 2.431, 2.384, 2.339, 2.296, 2.254, 2.214, 
+#       2.175, 2.138];
+
+#Arr = [3.5424, 3.4925, 3.4440, 3.3968, 3.3509, 3.3062, 3.2627, 3.2204, 3.1791, 3.1629, 3.1468, 3.1388, 
+#       3.1309, 3.1230, 3.1152, 3.0996, 3.0842, 3.0689, 3.0538, 3.0388, 3.0240, 3.0093, 2.9948, 2.9876, 
+#       2.9520, 2.8834, 2.8178, 2.7552, 2.6953, 2.6380, 2.5830, 2.5303, 2.4797, 2.4311, 2.3843, 2.3393, 
+#       2.2960, 2.2543, 2.2140, 2.1752];
+
+#Arr1 = np.linspace(Initial,Final,num=waveNUM)
+#pauser = 1;
 #for q in range(0,len(Arr)):
 
 
@@ -83,22 +106,34 @@ Queue 1
 ################################################################
 
 #initial absorption length
-AbsIN = 0.5
+#AbsIN = 0.1
 
 #final absorption length
-AbsFI = 50.0
+#AbsFI = 12
 
 #total number of jobs being submitted 
-jobNUM = 1000
+#jobNUM = 1000
 
 #array that contains the different abs lengths being tested                          
-Arr = np.linspace(AbsIN,AbsFI,num=jobNUM)
-# used to allow the submission of thousands of jobs, 20 at a time
+#Arr = np.linspace(AbsIN,AbsFI,num=jobNUM)
+
+#Fill array with random numbers between 0 and 100,000 to be used as random seeds
+ArrRand = np.random.randint(1000000, size=200)
+
+#range to loop over
+myrange = 100
+
+#starting seed 
+startingseed1 = 98110
+startingseed2 = 72722
+
+
 pauser = 1;
-for q in range(len(Arr)):
+for q in range(myrange):
+#for q in range(0,len(Arr)):
     
-    if (pauser % 20 == 0):
-        sleep(600);
+    if (pauser % 30 == 0):
+        sleep(3600);
 
     pauser = pauser + 1; 
     # Creating new file                                                              
@@ -108,11 +143,18 @@ for q in range(len(Arr)):
     with open('photest' '%s' '.mac' % q, 'r') as file :
         filedata = file.read()
 
-    # Replacing the target string                                                    
-    j = str(Arr[q])
+    # Replacing the target string
+        a = str(startingseed1)
+        b = str(ArrRand[2*q])
+        k = str(startingseed2)
+        l = str(ArrRand[2*q+1])
+        #j = str(Arr[q])
     #i = str(Initial)
-    i = str(AbsIN)
-    filedata = filedata.replace(i, j)
+        #i = str(AbsIN)
+    
+        #filedata = filedata.replace(i, j)
+        filedata = filedata.replace(k,l)
+        filedata = filedata.replace(a,b)
 
     # Writing out the new file                                                       
     with open('photest' '%s' '.mac' % q, 'w') as file:
@@ -145,6 +187,7 @@ for q in range(len(Arr)):
    
     
     print "\n"
+    #sleep(500);
     #print "Histos output dir: %s/%s"%(OutDir,ProdTag)
 
-#print(Arr)
+print(Arr)
