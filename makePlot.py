2053632c49aa31ec3170560866e54bcda4897b45
Avi Kahn <avi.fs.kahn@gmail.com>
2019-03-01 11:33:33 -0500

    Update makePlot.py
    V4: used to make "standard deviation in training measurements" plot

--- makePlot.py
+++ makePlot.py
@@ -7,9 +7,11 @@ plt.rcParams.update({'font.size': 18})
 
 
 #Reads in file
-df       = pd.read_csv("EJ260.csv", sep=",")
+df       = pd.read_csv("training_ej260.csv", sep=",")
 Waves    = df.Waves
-#Mu_eff   = df.MuEffective
+#Mu_eff   = df.Mu_eff
+
+# All after repair
 Avi_a    = df.Avi_a
 Avi_b    = df.Avi_b
 Avi_c    = df.Avi_c
@@ -25,20 +27,13 @@ Tim_b    = df.Tim_b
 Tim_c    = df.Tim_c
 Tim_d    = df.Tim_d
 
+#before repair - 6/14/18
 Avi2_a    = df.Avi2_a
 Avi2_b    = df.Avi2_b
 Avi2_c    = df.Avi2_c
 Avi2_d    = df.Avi2_d
 
-Avi3_a    = df.Avi3_a
-Avi3_b    = df.Avi3_b
-Avi3_c    = df.Avi3_c
-Avi3_d    = df.Avi3_d
 
-Kak_a     = df.Kak_a
-Kak_b     = df.Kak_b
-Kak_c     = df.Kak_c
-Kak_d     = df.Kak_d
 
 #Andres1 = np.mean( np.array([ Andres_1, Andres_2 ]), axis=0 ) # set V1, 0 days aft
 #Andres2 = np.mean( np.array([ Andres2_1, Andres2_2 ]), axis=0 )# set V3, 14 days aft
@@ -50,28 +45,71 @@ Kak_d     = df.Kak_d
 #Avi1   = np.mean( np.array([ Avi_1, Avi_2 ]), axis=0 )#V1, 468
 #Avi2   = np.mean( np.array([ Avi2_1, Avi2_2 ]), axis=0 )#V3, 154
 #Avi3   = np.mean( np.array([ Avi3_1, Avi3_2 ]), axis=0 )#V3, 410
+ 
+stdeva = [0]*len(Waves)
+stdevb = [0]*len(Waves)
+stdevc = [0]*len(Waves)
+stdevd = [0]*len(Waves)
 
-#stdevT = [0]*len(Waves)
+stdeva_1day = [0]*len(Waves)
+stdevb_1day = [0]*len(Waves)
+stdevc_1day = [0]*len(Waves)
+stdevd_1day = [0]*len(Waves)
+
 #stdV1  = [0]*len(Waves)
 #stdV3  = [0]*len(Waves)
-#for i in range(len(Waves)):
-#    pointV1 = []
+for i in range(len(Waves)):
+    pointa = []
+    pointa_1day = []
+    pointb = []
+    pointb_1day = []
+    pointc = []
+    pointc_1day = [] 
+    pointd = []
+    pointd_1day = []
 #    pointV3 = []
-#    pointV1.append(Andres1[i])
-#    pointV3.append(Andres2[i])
-#    pointV3.append(Andres3[i])
-#    pointV3.append(Andres4[i])
-#    pointV1.append(Kak1[i])
-#    pointV1.append(Kak2[i])
-#    pointV3.append(Kak3[i])
-#    pointV1.append(Avi1[i])
-#    pointV3.append(Avi2[i])
-#    pointV3.append(Avi3[i])
-#    point = pointV1 + pointV3
-#    stdevT[i] = np.std(point)
-#    stdV1[i]  = np.std(pointV1)
-#    stdV3[i]  = np.std(pointV3)
+    pointa.append(Avi_a[i])
+    pointa.append(Duan_a[i])
+    pointa.append(Tim_a[i])
+    pointa_1day.append(Avi_a[i])
+    pointa_1day.append(Duan_a[i])
+    pointa_1day.append(Tim_a[i])
+    pointa_1day.append(Avi2_a[i])
 
+    pointb.append(Avi_b[i])
+    pointb.append(Duan_b[i])
+    pointb.append(Tim_b[i])
+    pointb_1day.append(Avi_b[i])
+    pointb_1day.append(Duan_b[i])
+    pointb_1day.append(Tim_b[i])
+    pointb_1day.append(Avi2_b[i])
+
+    pointc.append(Avi_c[i])
+    pointc.append(Duan_c[i])
+    pointc.append(Tim_c[i])
+    pointc_1day.append(Avi_c[i])
+    pointc_1day.append(Duan_c[i])
+    pointc_1day.append(Tim_c[i])
+    pointc_1day.append(Avi2_c[i])
+
+    pointd.append(Avi_d[i])
+    pointd.append(Duan_d[i])
+    pointd.append(Tim_d[i])
+    pointd_1day.append(Avi_d[i])
+    pointd_1day.append(Duan_d[i])
+    pointd_1day.append(Tim_d[i])
+    pointd_1day.append(Avi2_d[i])
+
+#    point = pointV1 + pointV3
+    stdeva[i] = np.std(pointa)
+    stdevb[i] = np.std(pointb)
+    stdevc[i] = np.std(pointc)
+    stdevd[i] = np.std(pointd)
+
+    stdeva_1day[i] = np.std(pointa_1day)
+    stdevb_1day[i] = np.std(pointb_1day)
+    stdevc_1day[i] = np.std(pointc_1day)
+    stdevd_1day[i] = np.std(pointd_1day)
 #Mu = list(df.Mu)
 #E  = list(df.E)
 #Eff = np.asarray(E)
@@ -91,67 +129,30 @@ err = 0.4
 
 plt.figure(1)
 #plt.plot(Waves,Mu_eff)
-plt.subplot(221)
-plt.plot(Waves, Kak_a,'g',label='March 2017')
-plt.plot(Waves, Avi3_a,'b',label='April 2018')
-plt.plot(Waves, Avi2_a,'r',label='June 2018')
-plt.plot(Waves, Avi_a, 'm',linestyle='dashed', label='February 2019 (After repair)')
+#plt.subplot(121)
+plt.plot(Waves, stdeva, 'g', label='Side A')
+plt.plot(Waves, stdevb,'b',label='Side B')
+plt.plot(Waves, stdevc,'r',label='Side C')
+plt.plot(Waves, stdevd,'m', label='Side D')
 plt.legend(loc='upper right', ncol=2)
-plt.errorbar(Waves,Avi_a, yerr = err, errorevery=15, capsize = 3, ecolor= 'm', fmt = 'm',ls='-.') 
-plt.errorbar(Waves,Avi3_a, yerr = err, errorevery=15, capsize = 3, ecolor= 'b', fmt = 'b') 
+#plt.errorbar(Waves,stdeva, yerr = err, errorevery=50, capsize = 3, ecolor= 'b', fmt = 'b') 
 plt.xlabel('Wavelength ($nm$)')
-plt.ylabel('Transmission Efficiency')
-plt.ylim(84,90) 
-plt.xlim(500,600)
+plt.ylabel('Stdev in Transmission Efficiency (%)')
+#plt.ylim(84,89) 
+plt.xlim(400,600)
 #plt.ylim(1e-06,1e02)
-plt.title('EJ260-1X1P-N1 Reference Measurements: Side A')
-
-plt.subplot(222)
-#plt.plot([], [], ' ', label="Repaired Febuary 2019")
-plt.plot(Waves, Kak_b,'g',label='March 2017')
-plt.plot(Waves, Avi3_b,'b',label='April 2018')
-plt.plot(Waves, Avi2_b,'r',label='June 2018')
-plt.plot(Waves, Avi_b, 'm',linestyle='dashed', label='February 2019 (After repair)')
-plt.legend(loc='upper right',ncol = 2)
-plt.errorbar(Waves,Avi_b, yerr = err, errorevery=15, capsize = 3, ecolor= 'm', fmt = 'm',ls='-.') 
-plt.errorbar(Waves,Avi3_b, yerr = err, errorevery=15, capsize = 3, ecolor= 'b', fmt = 'b') 
-plt.xlabel('Wavelength ($nm$)')
-plt.ylabel('Transmission Efficiency')
-plt.ylim(82,88) 
-plt.xlim(500,600)
-#plt.ylim(1e-06,1e02)
-plt.title('EJ260-1X1P-N1 Reference Measurements: Side B')
-
-plt.subplot(223)
-#plt.plot([], [], ' ', label="Repaired Febuary 2019")
-plt.plot(Waves, Kak_c,'g',label='March 2017')
-plt.plot(Waves, Avi3_c,'b',label='April 2018')
-plt.plot(Waves, Avi2_c,'r',label='June 2018')
-plt.plot(Waves, Avi_c, 'm',linestyle='dashed', label='February 2019 (After repair)')
-plt.legend(loc='upper right',ncol = 2)
-plt.errorbar(Waves,Avi_c, yerr = err, errorevery=15, capsize = 3, ecolor= 'm', fmt = 'm',ls='-.') 
-plt.errorbar(Waves,Avi3_c, yerr = err, errorevery=15, capsize = 3, ecolor= 'b', fmt = 'b') 
-plt.xlabel('Wavelength ($nm$)')
-plt.ylabel('Transmission Efficiency')
-plt.ylim(84,90) 
-plt.xlim(500,600)
-#plt.ylim(1e-06,1e02)
-plt.title('EJ260-1X1P-N1 Reference Measurements: Side C')
-
-plt.subplot(224)
-#plt.plot([], [], ' ', label="Repaired Febuary 2019")
-plt.plot(Waves, Kak_d,'g',label='March 2017')
-plt.plot(Waves, Avi3_d,'b',label='April 2018')
-plt.plot(Waves, Avi2_d,'r',label='June 2018')
-plt.plot(Waves, Avi_d, 'm',linestyle='dashed', label='February 2019 (After repair)')
-plt.legend(loc='upper right',ncol = 2)
-plt.errorbar(Waves,Avi_d, yerr = err, errorevery=15, capsize = 3, ecolor= 'm', fmt = 'm',ls='-.') 
-plt.errorbar(Waves,Avi3_d, yerr = err, errorevery=15, capsize = 3, ecolor= 'b', fmt = 'b') 
-plt.xlabel('Wavelength ($nm$)')
-plt.ylabel('Transmission Efficiency')
-plt.ylim(82,88) 
-plt.xlim(500,600)
-#plt.ylim(1e-06,1e02)
-plt.title('EJ260-1X1P-N1 Reference Measurements: Side D')
+plt.title('Standard Deviation in Training Measurements')
+plt.legend(loc ='upper right')
+#plt.subplot(122)
+#plt.plot(Waves, stdeva, 'g', label='Avi')
+#plt.plot(Waves, stdevb,'b',label='Duan')
+#plt.plot(Waves, stdevc,'r',label='Tim')
+#plt.plot(Waves, stdevd,'g', label='Avi - 2018')
+#plt.legend(loc='upper right', ncol=2)
+#plt.errorbar(Waves,stdeva, yerr = err, errorevery=15, capsize = 3, ecolor= 'b', fmt = 'b') 
+#plt.xlabel('Wavelength ($nm$)', fontsize='x-small')
+#plt.ylabel('Stdev in Transmission Efficiency')
+#plt.suptitle('EJ260-1X1P-N1 Training Measurements: ')
+#plt.title('Standard Deviation including 2018 measurement')
 
 plt.show()
