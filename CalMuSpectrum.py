2883d19fe062ca0d5f9d500eb14cfaca4c5c86d5
Avi Kahn <avi.fs.kahn@gmail.com>
2019-03-18 16:58:28 -0400

    Update CalMuSpectrum.py
    V1: Used to make 10mm mueff vs wavelength plot WITH statistical uncertainty envelope.

--- CalMuSpectrum.py
+++ CalMuSpectrum.py
@@ -11,63 +11,88 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
 
 #Read in DataSet
-df       = pd.read_csv("10mm_LDR_data.csv", sep=",")
-Waves    = df.Wave
-Andres_1 = df.T_a_1
-Andres_2 = df.T_a_2
-Kak_1    = df.T_k_1
-Kak_2    = df.T_k_2
-Kak2_1   = df.T_k2_1
-Kak2_2   = df.T_k2_2
-Avi_1    = df.T_av_1
-Avi_2    = df.T_av_2
+df        = pd.read_csv("LDR_irr_10data.csv", sep=",")
+Waves     = df.Waves
+Kak_a     = df.T_K_a
+Kak_c     = df.T_K_c
+Andres_a  = df.T_A_a
+Andres_c  = df.T_A_c
+Andres2_a = df.T_A2_a
+Andres2_c = df.T_A2_c
+Avi_a     = df.T_AV_a
+Avi_c     = df.T_AV_c
+Andres3_a = df.T_A3_a
+Andres3_c = df.T_A3_c
+Avi2_a     = df.T_AV2_a
+Avi2_c     = df.T_AV2_c
 
-Andres = np.mean( np.array([ Andres_1, Andres_2 ]), axis=0 )
-Kak1   = np.mean( np.array([ Kak_1, Kak_2 ]), axis=0 )
-Kak2   = np.mean( np.array([ Kak2_1, Kak2_2 ]), axis=0 )
-Avi   = np.mean( np.array([ Avi_1, Avi_2 ]), axis=0 )
 
-TransE = np.mean(np.array([Andres, Kak1, Kak2, Avi]), axis=0)
+Kak   = np.mean( np.array([ Kak_a, Kak_c ]), axis=0 )
+Andres = np.mean( np.array([ Andres_a, Andres_c ]), axis=0 )
+Andres2 = np.mean( np.array([ Andres2_a, Andres2_c ]), axis=0 )
+Andres3 = np.mean( np.array([ Andres3_a, Andres3_c ]), axis=0 )
+Avi   = np.mean( np.array([ Avi_a, Avi_c ]), axis=0 )
+Avi2   = np.mean( np.array([ Avi2_a, Avi2_c ]), axis=0 )
+
+# Plus/Minus one sigma
+Kak = Kak + 0.7
+Andres = Andres + 0.7
+Andres2 = Andres2 + 0.7
+Andres3 = Andres3 + 0.7
+Avi = Avi + 0.7
+Avi2 = Avi2 + 0.7
+
+TransE = np.mean(np.array([Kak, Andres, Andres2, Andres3, Avi, Avi2]), axis=0)
+
+#don't want E > 100 or < 0 from the +- sigma systematic error curves
+TransE = TransE.clip(0,100) 
+
 L = len(Waves)
 MuResults  = [None]*L;
-start    = 0.0003
-stop     = 0.1
-total    = 4000000
-step     = (stop-start)/total
+start    = 0.58
+stop     = 1
+total    = 20000
+step     = (stop-start)/(total)
 minimum  = 100;
 Diff     = [None]*total;
 Results = [None]*total;
 
+def Eff10(x):
+    return 100 * np.exp(-0.1037-10.03*(x))
+def Eff6(x):
+    return 100 * np.exp(-0.10241-6.029*(x))
+def Eff4(x):
+    return 100 * np.exp(-0.1019-4.0212*(x))
+
+
 for i in range (len(Waves)):
 
     k = 0;
-
+    E_Meas = TransE[i]
     for mu in np.arange(start,stop,step):
          #10 mm fit function
-        Eff     = 100 * np.exp(-0.1037-10.03*(mu));
-        Diff[k]  = abs(TransE[i] - Eff);
-
+        Diff[k]  = abs(E_Meas - Eff10(mu));
+        #print('E_Meas[ ]= ', E_Meas, ' and Eff10(',mu,'mm)= ', Eff10(mu))
         #6 mm fit function
-        #Eff   = 100 * np.exp(-0.10241-6.029*(i))
-        #Diff[k]  = abs(TranData6[j] - Eff);
+        #Diff[k]  = abs(E_Meas - Eff6(mu));
 
         #4 mm fit function
-        #Eff   = 100 * np.exp(-0.1019-4.0212*(i))
-        #Diff[k]  = abs(TranData4[j] - Eff);
+        #Diff[k]  = abs(E_Meas - Eff4(mu));
 
         MuValue = k*step+start;
         Results[k] = MuValue;
         k = k + 1;
     mink      = Diff.index(min(Diff))
     MuResults[i] = Results[mink];
-    if Diff[mink] < 0.0001:
-        MuResults[i] = 100;#so a quick glance shows if there was a bad match 
+    if Diff[mink] > (E_Meas*0.0001): #comes from machine error of 0.01% (and ignored for very low values of efficiency) 
+        MuResults[i] = 100; #so a quick glance shows if there was a bad match 
 
 
-f = open('Muspec_10mm_LDR_V2.txt','w')
+f = open('Muspec_10mm_LDR_psigma.txt','w')
 
 print>>f, 'Wavelength,MuEffective'
 for w,mr in zip(Waves,MuResults):
     print>>f, "%.7f,%.7f" % (w, mr)
 
 f.close()
+
