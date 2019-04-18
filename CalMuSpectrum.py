87b13085e91e14c4841f1fb84508568637375126
Avi Kahn <avi.fs.kahn@gmail.com>
2019-04-18 03:20:49 -0400

    Update CalMuSpectrum.py
    V2: No longer compares to data during the first few weeks after irradiation.

--- CalMuSpectrum.py
+++ CalMuSpectrum.py
@@ -11,12 +11,12 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
 
 #Read in DataSet
-df        = pd.read_csv("LDR_irr_10data.csv", sep=",")
+df        = pd.read_csv("LDR_irr_4data.csv", sep=",")
 Waves     = df.Waves
-Kak_a     = df.T_K_a
-Kak_c     = df.T_K_c
-Andres_a  = df.T_A_a
-Andres_c  = df.T_A_c
+#Kak_a     = df.T_K_a
+#Kak_c     = df.T_K_c # still temporary damage
+#Andres_a  = df.T_A_a
+#Andres_c  = df.T_A_c
 Andres2_a = df.T_A2_a
 Andres2_c = df.T_A2_c
 Avi_a     = df.T_AV_a
@@ -26,39 +26,39 @@ Andres3_c = df.T_A3_c
 Avi2_a     = df.T_AV2_a
 Avi2_c     = df.T_AV2_c
 
-
-Kak   = np.mean( np.array([ Kak_a, Kak_c ]), axis=0 )
-Andres = np.mean( np.array([ Andres_a, Andres_c ]), axis=0 )
+#Kak   = np.mean( np.array([ Kak_a, Kak_c ]), axis=0 )
+#Andres = np.mean( np.array([ Andres_a, Andres_c ]), axis=0 )
 Andres2 = np.mean( np.array([ Andres2_a, Andres2_c ]), axis=0 )
 Andres3 = np.mean( np.array([ Andres3_a, Andres3_c ]), axis=0 )
 Avi   = np.mean( np.array([ Avi_a, Avi_c ]), axis=0 )
 Avi2   = np.mean( np.array([ Avi2_a, Avi2_c ]), axis=0 )
 
 # Plus/Minus one sigma
-Kak = Kak + 0.7
-Andres = Andres + 0.7
+#Kak = Kak - 0.7
+#Andres = Andres - 0.7
 Andres2 = Andres2 + 0.7
 Andres3 = Andres3 + 0.7
 Avi = Avi + 0.7
 Avi2 = Avi2 + 0.7
 
-TransE = np.mean(np.array([Kak, Andres, Andres2, Andres3, Avi, Avi2]), axis=0)
+TransE = np.mean(np.array([Andres2, Andres3, Avi, Avi2]), axis=0)
 
 #don't want E > 100 or < 0 from the +- sigma systematic error curves
 TransE = TransE.clip(0,100) 
 
 L = len(Waves)
 MuResults  = [None]*L;
-start    = 0.58
-stop     = 1
-total    = 20000
+start    = 0.00001
+stop     = 0.00005
+total    = 100000
 step     = (stop-start)/(total)
-minimum  = 100;
 Diff     = [None]*total;
 Results = [None]*total;
 
 def Eff10(x):
     return 100 * np.exp(-0.1037-10.03*(x))
+def Eff8(x):
+    return 100 * np.exp(-0.1046-8.0077*(x))
 def Eff6(x):
     return 100 * np.exp(-0.10241-6.029*(x))
 def Eff4(x):
@@ -70,14 +70,7 @@ for i in range (len(Waves)):
     k = 0;
     E_Meas = TransE[i]
     for mu in np.arange(start,stop,step):
-         #10 mm fit function
-        Diff[k]  = abs(E_Meas - Eff10(mu));
-        #print('E_Meas[ ]= ', E_Meas, ' and Eff10(',mu,'mm)= ', Eff10(mu))
-        #6 mm fit function
-        #Diff[k]  = abs(E_Meas - Eff6(mu));
-
-        #4 mm fit function
-        #Diff[k]  = abs(E_Meas - Eff4(mu));
+        Diff[k]  = abs(E_Meas - Eff4(mu));
 
         MuValue = k*step+start;
         Results[k] = MuValue;
@@ -88,7 +81,7 @@ for i in range (len(Waves)):
         MuResults[i] = 100; #so a quick glance shows if there was a bad match 
 
 
-f = open('Muspec_10mm_LDR_psigma.txt','w')
+f = open('Mu_4_819_p_corrected.txt','w')
 
 print>>f, 'Wavelength,MuEffective'
 for w,mr in zip(Waves,MuResults):
